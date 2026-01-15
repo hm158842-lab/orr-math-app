8b0c6c66bba90effca4becf885c18838a355ca72
leemk1103-gif <leemk1103@naver.com>
2026-01-15 18:37:24 +0900

    필터변경

--- netlify/functions/get-student.js
+++ netlify/functions/get-student.js
@@ -2,18 +2,17 @@ const { Client } = require('@notionhq/client');
 
 exports.handler = async (event) => {
   const notion = new Client({ auth: process.env.NOTION_KEY });
-  const databaseId = '2e88e00f84408099a3e5f0f3acaf5c96'; // ★ 원장님 DB ID (32자리) 확인!
+  // ★ 원장님 주소에서 제가 뽑아낸 '진짜 ID'입니다. (건드리지 마세요!)
+  const databaseId = '2e88e00f84408099a3e5f0f3acaf5c96'; 
 
   try {
     const name = event.queryStringParameters.name || '김도일';
     const response = await notion.databases.query({
       database_id: databaseId,
       filter: {
-        property: 'StudentId', // ★ 방금 만든 수식 칸을 검사합니다!
-        formula: {
-          string: {
-            contains: name
-          }
+        property: '이름',  // ★ 수식 말고 맨 앞 '이름' 칸을 봅니다.
+        title: {           // ★ 제목(Title) 속성에서
+          contains: name   // ★ '김도일'이 포함되어 있으면 다 가져옵니다. (기말성적 붙어있어도 OK)
         }
       }
     });
